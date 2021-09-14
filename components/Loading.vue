b452541b99af2638da808c135c9e0909e23861e5
korkt-kim <gnscott6@naver.com>
2021-09-14 23:16:40 +0900

    add customized loading component

---
+++ components/Loading.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if="loading" class="loading-page">
+    <img src="/loading.gif" class="loading" />
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    loading: false
+  }),
+  methods: {
+    start() {
+      this.loading = true;
+    },
+    finish() {
+      this.loading = false;
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.loading-page {
+  z-index: 2;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0.9);
+  .loading {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -20,4 +20,13 @@
   </v-row>
 </template>
 
-<script></script>
+<script>
+export default {
+  // mounted() {
+  //   this.$nextTick(() => {
+  //     this.$nuxt.$loading.start();
+  //     setTimeout(() => this.$nuxt.$loading.finish(), 2000);
+  //   });
+  // }
+};
+</script>
